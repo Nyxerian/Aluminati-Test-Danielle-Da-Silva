7612224d54d7e2579b25249df69a535b573bec6d
Nyxerian <63734982+Nyxerian@users.noreply.github.com>
2022-05-04 14:36:28 +0200

    Update Numbers.vue

--- src/components/Numbers.vue
+++ src/components/Numbers.vue
@@ -1,7 +1,13 @@
+<!--
+All variables, class names and function names are almost identical. I have renamed them to be more descriptive allowing 
+for easier reading of the code.
+-->
 <template>
-  <div>
-    <div class="number" :id="'number-'+number" v-for="number in n()" :key="number" @mouseover="hov(number)" @mouseout="reset">
-      {{number}}
+  <div class="mainContainer">
+    <div class="Numbercontainer">
+      <div class="number" :id="'number-'+number" v-for="number in n()" :key="number" @mouseover="hover(number)" @mouseout="reset">
+        {{number}}
+      </div>
     </div>
   </div>
 </template>
@@ -12,7 +18,7 @@ export default {
   {
     return {
       limit: this.$parent.limit,
-      numbers: []
+      allallNumbers: []
     }
   },
   watch: {
@@ -24,45 +30,58 @@ export default {
   methods: {
     n()
     {
-      let numbers = [];
-      for(var i = 0; i < this.limit; i++)
-      {
-        numbers = [...numbers, i];
-      }
-      return numbers.sort(() => Math.random() - 0.5);
+      let allNumbers = [];
+      //Changed range to be from 1 not 0 as per README instructions. Also changed var to let to match with the rest of the code.
+        for (let i = 1; i < this.limit; i++) {
+          allNumbers = [...allNumbers, i];
+          }
+          return allNumbers.sort(() => Math.random() - 0.5)
     },
-    hov(number)
+    hover(number)
     {
-      const nums = document.querySelectorAll('.number');
+      const numberSelect = document.querySelectorAll('.number');
 
-      for(let i = 0; i < nums.length; i++)
+      for(let i = 0; i < numberSelect.length; i++)
       {
-        const num = nums[i].textContent.trim();
+        const num = numberSelect[i].textContent.trim();
         if(number % num === 0)
         {
-          nums[i].classList.add('active')
-          console.log('divisor', num)
+          numberSelect[i].classList.add('active')
+          //Removed the console.log. Final code shouldnt contain console.logs
         }
       }
     },
     reset()
     {
-      const nums = document.querySelectorAll('.number');
-      nums.forEach(num => num.classList.remove('active'))
+      const numberSelect = document.querySelectorAll('.number');
+      numberSelect.forEach(num => num.classList.remove('active'))
     }
   }
 }
 </script>
 
+<!--
+Styling for additional containers to make numbers easier to read. Also changed background and highlight colours 
+to be less...violent on the eyes.
+-->
 <style scoped>
+  .mainContainer{
+  margin: 2em 0 0 0;
+  display: flex;
+  align-items: center;
+  }
+  .Numbercontainer {
+    display: flex-wrap-column;
+    justify-content: center;
+    width: 50%;
+  }
 	.number {
 		display: inline-block;
-		padding: 5px;
-		background-color: lightgrey;
-		margin: 5px;
+		padding: 10px;
+		background-color: rgb(244, 244, 244);
+		margin: 8px;
 	}
-
 	.active {
-		background-color: red;
+		background-color: rgba(149, 254, 154, 0.811);
 	}
 </style>
